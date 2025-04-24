d26150d867a2bfb6840d67c63f735377b54fd744
Carlos Carracedo garcía-villalba <ccg-v@hotmail.com>
2025-04-24 22:38:51 +0200

    ...

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -68,5 +68,5 @@ exec mysqld_safe
 # ------------------------------------------------------------------------------
 # [4] `mmysql_admin` is another admin tool (like `mariadb-admin`) that comes from
 #		the MySQL legacy tools. It allows us to gracefully stop the MariaDB server.
-#		Here we have to pass the root server because it has already been set in  
-#		the last SQL statement.
+#		Here we have to provide the root password because it has already been set 
+#		in the last SQL statement.
