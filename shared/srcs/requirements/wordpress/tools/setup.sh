447804977d6e06705523a1b210d2a297810e9ee8
Carlos Carracedo garcía-villalba <ccg-v@hotmail.com>
2025-05-02 02:07:25 +0200

    Error: 504 Gateway Time-out

--- shared/srcs/requirements/wordpress/tools/setup.sh
+++ shared/srcs/requirements/wordpress/tools/setup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Download and install WordPress, if it does not exist
+if [ ! -f /var/www/html/index.php ]; then
+    echo "Installing WordPress..."
+    mkdir -p /var/www/html
+    curl -O https://wordpress.org/latest.tar.gz
+    tar -xzf latest.tar.gz
+    mv wordpress/* /var/www/html/
+    rm -rf wordpress latest.tar.gz
+else
+    echo "WordPress already installed."
+fi
+
 # Copy sample config
 cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
@@ -10,4 +22,4 @@ sed -i "s/password_here/${MYSQL_PASSWORD}/" /var/www/html/wp-config.php
 sed -i "s/localhost/${WORDPRESS_DB_HOST}/" /var/www/html/wp-config.php
 
 # Start php-fpm in the foreground
-exec php-fpm7.4 -F
+exec /usr/sbin/php-fpm7.4 -F
